0b78ad54408a42bdb0ce61f121bb8468567214d2
Brendan Ward <kingbeebop@gmail.com>
2024-04-06 10:25:08 -0400

    app.tsx

---
+++ app/pages/_app.tsx
@@ -0,0 +1,24 @@
+// pages/_app.tsx
+import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+// import store from '../redux/store';
+// import Layout from '../components/Layout';
+// import ErrorBoundary from '../components/ErrorBoundary';
+import 'tailwindcss/tailwind.css';
+
+import '../styles/globals.css';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+
+  return (
+    <Provider store={store}>
+      {/* <ErrorBoundary fallback={<p>We&apos;re currently experiencing a problem with our servers.</p>}>
+        <Layout> */}
+          <Component {...pageProps} />
+        {/* </Layout>
+      </ErrorBoundary> */}
+    </Provider>
+  );
+};
+
+export default MyApp;
