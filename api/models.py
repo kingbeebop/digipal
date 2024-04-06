8ed7d15e074a20eb8df751deffb41e3f9f2028f8
OwenOrcan <owenorcan@gmail.com>
2024-04-06 11:38:03 -0400

    bryan: created immutable pet types, added add function to save pet types and incorporate error handling

--- api/models.py
+++ api/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+PET_TYPES = (
+    ('Dog', 'Dog'),
+    ('Cat', 'Cat'),
+    ('Dragon', 'Dragon')
+)
+
+
+def save(self, *args, **kwargs):
+    if self.pet_type not in [x[0] for x in self.PET_TYPES]:
+        raise ValueError('Invalid pet type')
+    super(Pet, self).save(*args, **kwargs)
+    
 
 class Pet(models.Model):
     name = models.CharField(max_length=100)
@@ -8,8 +20,12 @@ class Pet(models.Model):
     hunger = models.IntegerField()
     energy = models.IntegerField()
 
+
+# Fixed options for state only those that
+
     def __str__(self):
         return self.name
+
 
 """Example instance of a new pet
 new_pet = Pet(pet_type="Dog", state="Idle", name="Rex", hunger=5, energy=8)
