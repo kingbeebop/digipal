64949f1ebc33b8299dbf90d42d43c8abac6f6198
OwenOrcan <owenorcan@gmail.com>
2024-04-06 11:14:20 -0400

    added models.py file

---
+++ api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    pet_type = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    hunger = models.IntegerField()
+    energy = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+"""Example instance of a new pet
+new_pet = Pet(pet_type="Dog", state="Idle", name="Rex", hunger=5, energy=8)
+new_pet.save()
+"""
